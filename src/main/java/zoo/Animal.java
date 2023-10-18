8c39f24b01fc944ef3cfd969da57ddb6daa3c8c1
kamillabuda <kamillabuda7776@gmail.com>
2023-10-18 08:50:21 +0200

    hfgf

---
+++ src/main/java/zoo/Animal.java
@@ -0,0 +1,26 @@
+package zoo;
+
+public class Animal {
+
+    String spiece;
+
+    int age; // liczba calkowita
+
+    long hairCount; // liczba calkowita -2^32 - 2^32
+
+    double height; // liczba zmiennoprzecinkowa - 2 po przecinku
+
+    float weight; // liczba zmiennoprzecinkowa (pelna dokladnosc)
+
+    boolean isAlive; // zmienna logiczna true/false
+
+
+
+    public void speak() {
+        //soutv = System.out.println("spiece = " + spiece);
+
+        //sout = System.out.println("")
+        System.out.println("hello I am " + spiece);
+
+    }
+}
