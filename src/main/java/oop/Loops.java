7e157e250d83c60b28e361e17d42573cc261b815
kamillabuda <kamillabuda7776@gmail.com>
2023-10-25 08:37:37 +0200

    Kamil_ZSL_Java

--- src/main/java/oop/Loops.java
+++ src/main/java/oop/Loops.java
@@ -1,8 +1,13 @@
 package oop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Loops {
 
     static String[] tablica;
+
+    List<String> lista = new ArrayList<>();
 
     //psvm -> public static void main(String[] args) {
     public static void main(String[] args) {
---
+++ src/main/java/zoo/Owner.java
@@ -0,0 +1,36 @@
+package zoo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Owner {
+    public static void main(String[] args) {
+        Animal dog = new Animal("dog");
+
+        dog.speak();
+//        dog.feed("jedzenie");
+//        dog.feed("jedzenie", 2);
+
+        Food paroowa = new Food("parówka", 50000);
+        Food trutka = new Food("trutka", 50000);
+        Food marchewa = new Food("marchewka", 10000);
+        Food smaczek = new Food("smaczek", 5000);
+
+
+        List<Food> jedzonko = new ArrayList<>();
+
+        jedzonko.add(paroowa);
+        jedzonko.add(trutka);
+        jedzonko.add(marchewa);
+        jedzonko.add(smaczek);
+
+        for (Food food : jedzonko) {
+            if (food.foodType.equals("trutka")) {
+                System.out.println("sam se to zjedz!");
+            } else {
+                dog.feed(food);
+            }
+        }
+
+    }
+}
